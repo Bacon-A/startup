3d39e3e7d20d7d11186f9b57395154c9f5cb2a76
Alek Bacon <kendalika3@gmail.com>
2023-03-10 15:20:05 -0700

    Adjustments..

--- addUsers.js
+++ addUsers.js
@@ -1,5 +1,13 @@
 function addUser(name) {
     const table = document.querySelector('#user-table tbody');
+  
+    // Check if the name already exists in the user table
+    const existingNames = Array.from(table.querySelectorAll('td:first-child')).map(td => td.textContent.trim());
+    if (existingNames.includes(name.trim())) {
+      console.log('User with this name already exists.');
+      return;
+    }
+  
     const row = document.createElement('tr');
     const nameCell = document.createElement('td');
     const viewPostsCell = document.createElement('td');
@@ -8,20 +16,13 @@ function addUser(name) {
     nameCell.textContent = name;
     link.textContent = 'View Posts';
     link.href = '#';
-    viewPostsCell.classList.add('view-posts'); // Add class to the td element
-  
     viewPostsCell.appendChild(link);
   
     row.appendChild(nameCell);
     row.appendChild(viewPostsCell);
   
     table.appendChild(row);
-
-
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    users.push({ name });
-    localStorage.setItem('users', JSON.stringify(users));
-}
+  }
 
 function init() {
     const users = JSON.parse(localStorage.getItem('users')) || [];
