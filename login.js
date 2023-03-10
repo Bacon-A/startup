373a78bad4787b34a7f263226096026bf236a2cf
Alek Bacon <kendalika3@gmail.com>
2023-03-10 14:05:17 -0700

    Update to add Register

--- login.js
+++ login.js
@@ -2,7 +2,19 @@
 //Unsecured
 //Using local storage
 function login() {
-  const nameEl = document.querySelector("#name");
+  const nameEl = document.querySelector("#username");
   localStorage.setItem("userName", nameEl.value);
   window.location.href = "john_posts.html";
 }
+
+function register() {
+    const nameEl = document.querySelector("#regname");
+    const username = nameEl.value.trim();
+  
+    // Perform validation on the username
+    // If validation fails, display an error message to the user and prevent further execution of the function.
+  
+    // Store the username in local storage or send it to a server-side script to store in a database.
+    localStorage.setItem("userName", username);
+    window.location.href = "john_posts.html";
+  }
