7c228e97af593a18623968a0af560eb24f2efae0
Alek Bacon <kendalika3@gmail.com>
2023-03-10 17:16:30 -0700

    Persistent users, persistent posts

--- addUsers.js
+++ addUsers.js
@@ -16,17 +16,41 @@ function addUser(name) {
     nameCell.textContent = name;
     link.textContent = 'View Posts';
     link.href = `#${name}-posts`;
-    //viewPostsCell.appendChild(link);
-    //viewPostsCell.classList.add('view-posts');
+    viewPostsCell.style.textAlign = 'right'; // set text-align property to right
   
+    viewPostsCell.appendChild(link); // append link to viewPostsCell
     row.appendChild(nameCell);
     row.appendChild(viewPostsCell);
   
     table.appendChild(row);
   
     // Add the new user to the list of users in local storage
-    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const users = JSON.parse(sessionStorage.getItem('users')) || [];
     users.push({ name });
-    localStorage.setItem('users', JSON.stringify(users));
+    sessionStorage.setItem('users', JSON.stringify(users));
   }
   
+  function loadUsers() {
+    const table = document.querySelector('#user-table tbody');
+    const users = JSON.parse(sessionStorage.getItem('users')) || [];
+  
+    for (const user of users) {
+      const row = document.createElement('tr');
+      const nameCell = document.createElement('td');
+      const viewPostsCell = document.createElement('td');
+      const link = document.createElement('a');
+  
+      nameCell.textContent = user.name;
+      link.textContent = 'View Posts';
+      link.href = `#${user.name}-posts`;
+  
+      viewPostsCell.style.textAlign = 'right';
+      viewPostsCell.appendChild(link);
+  
+      row.appendChild(nameCell);
+      row.appendChild(viewPostsCell);
+  
+      table.appendChild(row);
+    }
+  }
+  window.addEventListener('load', loadUsers);
